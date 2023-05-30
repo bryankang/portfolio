ee298bbc74f50f4dfccd56ab3b9886602af11606
bryan <bryankang33@gmail.com>
2023-05-29 21:25:07 -0400

    typo

--- src/components/company-summaries/tr-summary.tsx
+++ src/components/company-summaries/tr-summary.tsx
@@ -18,9 +18,9 @@ export const TrSummary: FC = () => {
     <CompanySummary brand={<Brand />}>
       TrainerRoad provides training plans and workouts for cyclists. Think
       Peloton but targeted primarily to semi to professional athletes. Instead
-      of a all-in-one bicycle, customers typically use a smart trainer which
+      of an all-in-one bicycle, customers typically use a smart trainer which
       they can attach to their existing bicycles. The TrainerRoad apps
-      communicate with the start trainer in order to provide a workout
+      communicate with the smart trainer in order to provide a workout
       experience. I worked as a frontend developer and built features on the
       desktop app (Electron) and mobile app (React Native).
     </CompanySummary>
